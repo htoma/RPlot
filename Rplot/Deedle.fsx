e640e8a18fabea8e226ebb268e886f2157ff3dc0
htoma <horia.toma@availpro.com>
2016-06-24 15:31:34 +0200

    First version

---
+++ Rplot/Deedle.fsx
@@ -0,0 +1,55 @@
+﻿#load "../packages/FsLab.0.3.20/FsLab.fsx"
+#load "../packages/RProvider.1.1.20/RProvider.fsx"
+
+open System
+open Deedle
+open FSharp.Charting
+
+// Create from sequence of keys and sequence of values
+let dates  = 
+  [ DateTime(2013,1,1)
+    DateTime(2013,1,4) 
+    DateTime(2013,1,8) ]
+let values = 
+  [ 10.0; 20.0; 30.0 ]
+let first = Series(dates, values)
+
+// Create from a single list of observations
+Series.ofObservations
+  [ DateTime(2013,1,1) => 10.0
+    DateTime(2013,1,4) => 20.0
+    DateTime(2013,1,8) => 30.0 ]
+
+// Shorter alternative to 'Series.ofObservations'
+series [ 1 => 1.0; 2 => 2.0 ]
+
+// Create series with implicit (ordinal) keys
+Series.ofValues [ 10.0; 20.0; 30.0 ]
+
+/// Generate date range from 'first' with 'count' days
+let dateRange (first:System.DateTime) count = seq {for i in 0..(count-1) -> first.AddDays(float i)}
+
+/// Generate 'count' number of random doubles
+let rand count = 
+    let rnd = System.Random()
+    seq {for i in 0..(count-1) -> rnd.NextDouble()}
+
+// A series with values for 10 days 
+let second = Series(dateRange (DateTime(2013,1,1)) 10, rand 10)
+
+let df1 = Frame(["first"; "second"], [first; second])
+
+// The same as previously
+let df2 = Frame.ofColumns ["first" => first; "second" => second]
+
+// Transposed - here, rows are "first" and "second" & columns are dates
+let df3 = Frame.ofRows ["first" => first; "second" => second]
+
+// Create from individual observations (row * column * value)
+let df4 = 
+  [ ("Monday", "Tomas", 1.0); ("Tuesday", "Adam", 2.1)
+    ("Tuesday", "Tomas", 4.0); ("Wednesday", "Tomas", -5.4) ]
+  |> Frame.ofValues
+
+let msftCsv = Frame.ReadCsv(__SOURCE_DIRECTORY__ + "../data/msft.csv")
+let fbCsv = Frame.ReadCsv(__SOURCE_DIRECTORY__ + "/data/stocks/FB.csv")
---
+++ Rplot/Script.fsx
@@ -0,0 +1,46 @@
+﻿#load "../packages/FsLab.0.3.20/FsLab.fsx"
+#load "../packages/RProvider.1.1.20/RProvider.fsx"
+
+#load "ggplot.fs"
+
+open RProvider
+open RProvider.ggplot2
+open Deedle
+
+open ggplot
+
+open RProvider.datasets
+let mtc = R.mtcars.GetValue<Frame<string, string>>()
+
+let (++) (plot1:RDotNet.SymbolicExpression) (plot2:RDotNet.SymbolicExpression) = 
+    R.``+``(plot1, plot2) 
+
+R.ggplot(
+    namedParams[
+        "data", box mtc; 
+        "mapping", box (
+            R.aes__string(x="disp", y="drat"))])
+++ R.geom__point()
+
+G.ggplot(mtc, G.aes(x="disp", y="drat"))
+++ R.geom__point()
+
+
+let iris = R.iris.GetValue<Frame<string, string>>()
+
+let sizeSettings () =
+    R.theme(namedParams["axis.text", R.element__text(namedParams["size", 12])])
+    ++ R.theme(namedParams["legend.text", R.element__text(namedParams["size", 12])])
+    ++ R.theme(namedParams["axis.title", R.element__text(namedParams["size", 14])])
+    ++ R.theme(namedParams["plot.title", R.element__text(namedParams["size", 18])])
+
+// Create a plot
+G.ggplot(iris, G.aes(x="Sepal.Length", y="Sepal.Width",colour="Petal.Length"))
+++ R.geom__point(namedParams["size", 4])
+++ R.theme__bw()
+++ R.scale__color__gradient(
+    namedParams["low", "blue"; "high", "green"])
+++ R.ggtitle("Iris dataset")
+++ R.xlab("Sepal length")
+++ R.ylab("Sepal width")
+++ sizeSettings()
